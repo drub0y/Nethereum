fd99d1eff40710b628d6fed7e937c1d6bd769995
Drew Marsh <drub0y@hotmail.com>
2023-01-16 10:43:32 -0800

    Initial r/w perf opt of WebSocketClient

--- src/Nethereum.JsonRpc.WebSocketClient/WebSocketClient.cs
+++ src/Nethereum.JsonRpc.WebSocketClient/WebSocketClient.cs
@@ -9,6 +9,7 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 #if NETSTANDARD2_0_OR_GREATER || NETCOREAPP3_1_OR_GREATER || NET461_OR_GREATER || NET5_0_OR_GREATER
 using Microsoft.Extensions.Logging;
 #endif
@@ -17,7 +18,13 @@ namespace Nethereum.JsonRpc.WebSocketClient
 {
     public class WebSocketClient : ClientBase, IDisposable, IClientRequestHeaderSupport
     {
-        private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1, 1);
+        private static readonly Object BatchRequestId = new Object();
+
+        private readonly SemaphoreSlim _semaphoreSlimRead = new SemaphoreSlim(1, 1);
+        private readonly SemaphoreSlim _semaphoreSlimWrite = new SemaphoreSlim(1, 1);
+
+        private readonly ConcurrentDictionary<Object, TaskCompletionSource<MemoryStream>> _pendingRequests =
+            new ConcurrentDictionary<Object, TaskCompletionSource<MemoryStream>>();
 
         public Dictionary<string, string> RequestHeaders { get; set; } = new Dictionary<string, string>();
         protected string Path { get; set; }
@@ -59,7 +66,7 @@ namespace Nethereum.JsonRpc.WebSocketClient
                 if (_clientWebSocket != null && (_clientWebSocket.State == WebSocketState.Open || _clientWebSocket.State == WebSocketState.Connecting))
                 {
 
-                    await _semaphoreSlim.WaitAsync().ConfigureAwait(false);
+                    await Task.WhenAll(_semaphoreSlimRead.WaitAsync(), _semaphoreSlimRead.WaitAsync()).ConfigureAwait(false);
                     await _clientWebSocket.CloseOutputAsync(webSocketCloseStatus, status, timeOutToken).ConfigureAwait(false);
                     while (_clientWebSocket.State != WebSocketState.Closed && !timeOutToken.IsCancellationRequested) ;
                 }
@@ -67,7 +74,8 @@ namespace Nethereum.JsonRpc.WebSocketClient
             }
             finally
             {
-                _semaphoreSlim.Release();
+                _semaphoreSlimRead.Release();
+                _semaphoreSlimWrite.Release();
             }
 
         }
@@ -120,59 +128,111 @@ namespace Nethereum.JsonRpc.WebSocketClient
             }
         }
 
-        public async Task<MemoryStream> ReceiveFullResponseAsync(ClientWebSocket client)
+        public async Task ReceiveFullResponseAsync(ClientWebSocket client, Object requestId, CancellationToken cancellationToken)
         {
             var readBufferSize = 512;
-            var memoryStream = new MemoryStream();
+            var memoryStream = new MemoryStream(readBufferSize);
 
             var buffer = new byte[readBufferSize];
             var completedMessage = false;
 
-            while (!completedMessage)
+            try 
             {
-                var receivedResult = await ReceiveBufferedResponseAsync(client, buffer).ConfigureAwait(false);
-                var bytesRead = receivedResult.Count;
-                if (bytesRead > 0)
-                {
-                    memoryStream.Write(buffer, 0, bytesRead);
-                    var lastByte = buffer[bytesRead - 1];
+                await _semaphoreSlimRead.WaitAsync().ConfigureAwait(false);
 
-                    if (lastByte == 10 || receivedResult.EndOfMessage)  //return signaled with a line feed / or just less than the full message
-                    {
-                        completedMessage = true;
-                    }
-                }
-                else
+                while (!completedMessage)
                 {
-                    // We have had a response already and EndOfMessage
-                    if(receivedResult.EndOfMessage)
+                    var receivedResult = await ReceiveBufferedResponseAsync(client, buffer).ConfigureAwait(false);
+                    var bytesRead = receivedResult.Count;
+                    if (bytesRead > 0)
                     {
-                        completedMessage = true;
+                        memoryStream.Write(buffer, 0, bytesRead);
+                        var lastByte = buffer[bytesRead - 1];
+
+                        if (lastByte == 10 || receivedResult.EndOfMessage)  //return signaled with a line feed / or just less than the full message
+                        {
+                            completedMessage = true;
+                        }
+                    }
+                    else
+                    {
+                        // We have had a response already and EndOfMessage
+                        if(receivedResult.EndOfMessage)
+                        {
+                            completedMessage = true;
+                        }
                     }
                 }
             }
+            finally
+            {
+                _semaphoreSlimRead.Release();
+            }
 
-            if (memoryStream.Length == 0) return await ReceiveFullResponseAsync(client).ConfigureAwait(false); //empty response
-            return memoryStream;
+            TaskCompletionSource<MemoryStream> responseTaskCompletionSource = null;
+
+            // If this is a batch request, then we want to resolve the first pending batch request otherwise we 
+            // resolve the request with the given id
+            if(Object.ReferenceEquals(WebSocketClient.BatchRequestId, requestId)) {
+                _pendingBatchRequests.TryPeek(out responseTaskCompletionSource);
+            }
+            else 
+            {
+                _pendingRequests.TryGetValue(requestId, out responseTaskCompletionSource);
+            }
+
+            // Get the pending request and, only if it still exists, try to resolve it with the response
+            if(responseTaskCompletionSource != null) 
+            {
+                // Check if we got an non-empty response and, if so, complete the TaskCompletionSource with it
+                if (memoryStream.Length > 0) {
+                    responseTaskCompletionSource.SetResult(memoryStream);
+
+                    return;
+                }
+
+                // We received an empty response, asynchronously recurse to receive next response
+                memoryStream.Dispose();
+                
+                await ReceiveFullResponseAsync(client, requestId, cancellationToken).ConfigureAwait(false);
+            }
         }
 
         protected override async Task<RpcResponseMessage> SendAsync(RpcRequestMessage request, string route = null)
         {
+            var responseTaskCompletionSource = new TaskCompletionSource<MemoryStream>();
+            
+            if(!this._pendingRequests.TryAdd(request.Id, responseTaskCompletionSource)) {
+                throw new InvalidOperationException($"A request with the specified id is already pending: {request.Id}");
+            }
+
             var logger = new RpcLogger(_log);
+
             try
             {
-                await _semaphoreSlim.WaitAsync().ConfigureAwait(false);
                 var rpcRequestJson = JsonConvert.SerializeObject(request, JsonSerializerSettings);
                 var requestBytes = new ArraySegment<byte>(Encoding.UTF8.GetBytes(rpcRequestJson));
                 logger.LogRequest(rpcRequestJson);
-                var cancellationTokenSource = new CancellationTokenSource();
-                cancellationTokenSource.CancelAfter(ConnectionTimeout);
 
                 var webSocket = await GetClientWebSocketAsync().ConfigureAwait(false);
-                await webSocket.SendAsync(requestBytes, WebSocketMessageType.Text, true, cancellationTokenSource.Token)
-                    .ConfigureAwait(false);
+                var cancellationTokenSource = new CancellationTokenSource();
 
-                using (var memoryData = await ReceiveFullResponseAsync(webSocket).ConfigureAwait(false))
+                try
+                {
+                    await _semaphoreSlimWrite.WaitAsync().ConfigureAwait(false);
+                    cancellationTokenSource.CancelAfter(ConnectionTimeout);
+
+                    await webSocket.SendAsync(requestBytes, WebSocketMessageType.Text, true, cancellationTokenSource.Token)
+                        .ConfigureAwait(false);
+                }
+                finally
+                {
+                    _semaphoreSlimWrite.Release();
+                }
+
+                ReceiveFullResponseAsync(webSocket, request.Id, cancellationTokenSource.Token);
+                
+                using(var memoryData = await responseTaskCompletionSource.Task.ConfigureAwait(false))
                 {
                     memoryData.Position = 0;
                     using (var streamReader = new StreamReader(memoryData))
@@ -189,31 +249,49 @@ namespace Nethereum.JsonRpc.WebSocketClient
             {
                 var exception = new RpcClientUnknownException("Error occurred when trying to web socket requests(s): " + request.Method, ex);
                 logger.LogException(exception);
+
                 throw exception;
             }
             finally
             {
-                _semaphoreSlim.Release();
+                // Always clear the pending request
+                this._pendingRequests.TryRemove(request.Id, out _);
             }
         }
 
         protected async override Task<RpcResponseMessage[]> SendAsync(RpcRequestMessage[] requests)
         {
+            var responseTaskCompletionSource = new TaskCompletionSource<MemoryStream>();
+            
+            _pendingBatchRequests.Enqueue(responseTaskCompletionSource);
+            
             var logger = new RpcLogger(_log);
+
             try
             {
-                await _semaphoreSlim.WaitAsync().ConfigureAwait(false);
                 var rpcRequestJson = JsonConvert.SerializeObject(requests, JsonSerializerSettings);
                 var requestBytes = new ArraySegment<byte>(Encoding.UTF8.GetBytes(rpcRequestJson));
                 logger.LogRequest(rpcRequestJson);
-                var cancellationTokenSource = new CancellationTokenSource();
-                cancellationTokenSource.CancelAfter(ConnectionTimeout);
-
+                
                 var webSocket = await GetClientWebSocketAsync().ConfigureAwait(false);
-                await webSocket.SendAsync(requestBytes, WebSocketMessageType.Text, true, cancellationTokenSource.Token)
-                    .ConfigureAwait(false);
+                var cancellationTokenSource = new CancellationTokenSource();
 
-                using (var memoryData = await ReceiveFullResponseAsync(webSocket).ConfigureAwait(false))
+                try
+                {
+                    await _semaphoreSlimWrite.WaitAsync().ConfigureAwait(false);
+                    cancellationTokenSource.CancelAfter(ConnectionTimeout);
+                
+                    await webSocket.SendAsync(requestBytes, WebSocketMessageType.Text, true, cancellationTokenSource.Token)
+                        .ConfigureAwait(false);
+                }
+                finally
+                {
+                    _semaphoreSlimWrite.Release();
+                }
+
+                ReceiveFullResponseAsync(webSocket, WebSocketClient.BatchRequestId, cancellationTokenSource.Token);
+
+                using (var memoryData = await responseTaskCompletionSource.Task.ConfigureAwait(false))
                 {
                     memoryData.Position = 0;
                     using (var streamReader = new StreamReader(memoryData))
@@ -233,7 +311,8 @@ namespace Nethereum.JsonRpc.WebSocketClient
             }
             finally
             {
-                _semaphoreSlim.Release();
+                // Always clear the pending batch request
+                this._pendingBatchRequests.TryDequeue(out _);
             }
         }
 
